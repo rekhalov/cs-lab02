40893ce138be9b07784b788c815436545ec58725
Alice (RekhalovAS) <rekhalovdrusya52@gmail.com>
2022-02-28 17:08:46 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@ using namespace std;
 int main()
 {
 cout << "Enter A and B: ";
-int a, b, c;
+int a, b, c, d;
 cin >> a >> b;
 c = a + b;
+d = a - b;
 cout << c;
+cout << endl;
+cout << d;
 return 0;
 }
